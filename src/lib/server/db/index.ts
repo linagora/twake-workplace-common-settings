5b35c02761ab512c28885716d6db9cf533249143
Khaled FERJANI <kferjani@linagora.com>
2025-06-24 22:04:16 +0200

    🔧 chore: handle db init on build

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,8 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { building } from '$app/environment';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_URL && !building) {
+	throw new Error('DATABASE_URL is not set');
+}
 
 const client = postgres(env.DATABASE_URL);
 
