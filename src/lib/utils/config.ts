045114663646c629cadf7071e544eaed3f617617
Khaled Ferjani <kferjani@linagora.com>
2025-09-17 10:56:20 +0200

    🐛 fix: retry connecting/publishing if the RabbitMQ connection is closed (#9)
    * 🐛 fix: retry connecting/publishing if the connection is closed
    
    * 🧪 fix: update unit tests
    
    * 🐛 fix: do not wait for message notification being sent in API

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -4,6 +4,7 @@ export const DEFAULT_RABBITMQ_URL = 'amqp://localhost';
 export const DEFAULT_RABBITMQ_EXCHANGE = 'settings';
 export const DEFAULT_RABBITMQ_MAX_RETRIES = 3;
 export const DEFAULT_RABBITMQ_RETRY_DELAY = 1000;
+export const DEFAULT_RABBITMQ_CONNECTION_RETRY_DELAY = 5000;
 export const DEFAULT_SETTINGS_INPUT_QUEUE = 'user.settings.input';
 export const DEFAULT_SETTINGS_INPUT_ROUTING_KEY = 'user.settings.update';
 export const DEFAULT_SETTINGS_OUTPUT_ROUTING_KEY = 'user.settings.updated';
